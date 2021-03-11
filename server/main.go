74fc1394a8dbdd590069f6d78ab5673006130b3c
Roman Levin <romanlevin@gmail.com>
2021-03-11 12:11:08 +0100

    Graceful shutdown

--- server/main.go
+++ server/main.go
@@ -5,25 +5,61 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":12345")
+	// Catch SIGINT to shut down gracefully
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	defer signal.Stop(signals)
 
+	lis, err := net.Listen("tcp", ":12345")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
-	gRPCServer := grpc.NewServer()
 
 	jobServer, err := newServer()
 	if err != nil {
 		log.Fatalf("error creating jobs server: %s", err.Error())
 	}
 
+	gRPCServer := grpc.NewServer()
 	pb.RegisterJobsServer(gRPCServer, jobServer)
+
+	stopped := make(chan struct{})
+	go func() {
+		if err := gRPCServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+		close(stopped)
+	}()
+
 	log.Print("Serving gRPC service")
-	if err := gRPCServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+
+	// Wait for an interrupt
+	select {
+	case <-signals:
+		break
+	}
+
+	log.Print("Shutting down server")
+
+	// Start graceful shutdown
+	go func() {
+		gRPCServer.GracefulStop()
+	}()
+
+	// Force shutdown if GraceStop is taking a while
+	t := time.NewTimer(10 * time.Second)
+	select {
+	case <-t.C:
+		gRPCServer.Stop()
+		log.Print("Timed out, forcing a shutdown")
+	case <-stopped:
+		t.Stop()
+		log.Print("Server shut down gracefully")
 	}
 }
